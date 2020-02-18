8e96f915763cf7c431243f12cc3eb637087b66c8
Matthew Kaufman <matthew@eeph.com>
2020-02-18 09:56:20 -0800

    cleaner command shell parser

--- src/main/java/at/matthew/dbexample/Main.java
+++ src/main/java/at/matthew/dbexample/Main.java
@@ -1,11 +1,9 @@
 package at.matthew.dbexample;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Main {
-    private static final String searchPattern = "[^\"\\s]+|\"(\\\\.|[^\\\\\"])*\"";   // find unquoted or quoted (including escaped) tokens
-    private static final String stripPattern = "^\"|\"$";   // find leading or trailing quote
-
     public static void main(String[] args) {
         System.err.println("Welcome to dbexample!");
 
@@ -15,26 +13,21 @@ class Main {
         //noinspection InfiniteLoopStatement
         while (true) {
             String inputString = System.console().readLine();
-            Scanner scanner = new Scanner(inputString);
-            String cmd = scanner.findInLine(searchPattern);
-            String key = scanner.findInLine(searchPattern);
-            if (key != null)
-                key = key.replaceAll(stripPattern, "");
-            String value = scanner.findInLine(searchPattern);
-            if (value != null)
-                value = value.replaceAll(stripPattern, "");
+            ArrayList<String> tokens = tokenizeOptionallyQuoted(inputString);
+            int argc = tokens.size();
 
-
-            if (cmd == null) {
+            if (argc < 1) {
                 System.err.println("CMD [key [value]]");
             }
             else {
-                switch (cmd.toLowerCase()) {
+                String cmd = tokens.get(0).toLowerCase();
+                switch (cmd) {
                     case "get":
-                        if (key == null) {
+                        if (argc != 2) {
                             System.err.println("usage: GET <key>");
                         }
                         else {
+                            String key = tokens.get(1);
                             String result = db.get(key);
                             if (result == null)
                                 System.err.println("key \"" + key + "\" not found");
@@ -43,41 +36,60 @@ class Main {
                         }
                         break;
                     case "set":
-                        if (key == null || value == null) {
+                        if (argc != 3) {
                             System.err.println("usage: SET <key> <value>");
                         }
                         else {
+                            String key = tokens.get(1);
+                            String value = tokens.get(2);
                             db.set(key, value);
                         }
                         break;
                     case "unset":
-                        if (key == null) {
+                        if (argc != 2) {
                             System.err.println("usage: UNSET <key>");
                         }
                         else {
+                            String key = tokens.get(1);
                             db.unset(key);
                         }
                         break;
                     case "exists":
-                        if (key == null) {
+                        if (argc != 2) {
                             System.err.println("usage: EXISTS <key>");
                         }
                         else {
+                            String key = tokens.get(1);
                             boolean result = db.exists(key);
                             System.out.println(result ? "YES" : "NO");
                         }
                         break;
                     case "begin":
-                        db.begin();
+                        if (argc != 1) {
+                            System.err.println("usage: BEGIN");
+                        }
+                        else {
+                            db.begin();
+                        }
                         break;
                     case "rollback":
-                        if (!db.rollback()) {
-                            System.err.println("unable to roll back, not in transaction");
+                        if (argc != 1) {
+                            System.err.println("usage: ROLLBACK");
+                        }
+                        else {
+                            if (!db.rollback()) {
+                                System.err.println("unable to roll back, not in transaction");
+                            }
                         }
                         break;
                     case "commit":
-                        if (!db.commit())
-                            System.err.println("unable to commit, not in transaction");
+                        if (argc != 1) {
+                            System.err.println("usage: COMMIT");
+                        }
+                        else {
+                            if (!db.commit())
+                                System.err.println("unable to commit, not in transaction");
+                        }
                         break;
                     default:
                         System.out.println("CMD [key [value]] where CMD is one of GET, SET, UNSET, EXISTS, BEGIN, ROLLBACK, COMMIT");
@@ -85,5 +97,22 @@ class Main {
                 }
             }
         }
+    }
+
+
+    private static ArrayList<String> tokenizeOptionallyQuoted(String input) {
+        final String searchPattern = "[^\"\\s]+|\"(\\\\.|[^\\\\\"])*\"";   // find unquoted or quoted (including escaped) tokens
+        final String stripPattern = "^\"|\"$";   // find leading or trailing quote
+
+        String token;
+        Scanner scanner = new Scanner(input);
+        ArrayList<String> result = new ArrayList<>();
+
+        while ((token = scanner.findInLine(searchPattern)) != null) {
+            token = token.replaceAll(stripPattern, "");
+            result.add(token);
+        }
+
+        return result;
     }
 }
